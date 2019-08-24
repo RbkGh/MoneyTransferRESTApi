f2739c299e46dcb0e8e21be5cf75427710614dc6
Ace-Rbk <rodney.kboachie@gmail.com>
2019-08-24 19:17:09 +0000

    Added service package

---
+++ src/main/java/com/revolut/MoneyTransferAPIMainApp.java
@@ -0,0 +1,13 @@
+package com.revolut;
+
+/**
+ * author: acerbk
+ * Date: 2019-08-24
+ * Time: 19:00
+ */
+public class MoneyTransferAPIMainApp {
+
+    public static void main(String[] args) {
+
+    }
+}
